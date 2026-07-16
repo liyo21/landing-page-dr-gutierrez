37d1ee3442a23e737f181ab1aa649bb2bc867c1f
Eliyonai Molero <emolerop.srv@chileatiende.cl>
2026-07-15 22:11:19 -0400

    chore: se corrige configuracion de assets

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), tailwindcss()],
-  base: '/landing-page-dr-gutierrez/',
+  base: '/',
   resolve: {
     alias: {
       "@": "/src",
